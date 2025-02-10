42ce2081e776bb746ffd565b43897fec97f64b63
Punit Joshi <52650392+Punitt15@users.noreply.github.com>
2025-02-10 22:30:37 +0530

    React Instead of MERN Stack

--- src/app/portfoliov2/Bio.jsx
+++ src/app/portfoliov2/Bio.jsx
@@ -8,7 +8,7 @@ const Bio = ({ theme }) => {
             <div className='w-full md:max-w-[1140px] 2xl:max-w-[1440px] flex flex-col gap-7 md:gap-12 px-4 md:px-0'>
                 <motion.h1 initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} className='text-4xl md:text-6xl 2xl:text-8xl font-black'>ME</motion.h1>
                 <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='text-sm md:text-base md:font-light 2xl:font-normal 2xl:text-lg'>
-                    Software Developer with over 3 years of experience as MERN Stack Developer, eager to leverage technical
+                    Software Developer with over 3 years of experience as React Developer, eager to leverage technical
                     expertise and problem-solving skills in a dynamic, growth-oriented environment. Seeking a full-time opportunity
                     to contribute to innovative projects and collaborate with talented teams to deliver high-quality
                     software solutions.
